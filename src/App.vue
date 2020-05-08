04058f3e707abef273c52e5bc86e17075bdc8d61
ko-7 <shimura21ngs@gmail.com>
2020-05-08 23:10:41 +0900

    add top

--- src/App.vue
+++ src/App.vue
@@ -17,21 +17,21 @@
     <Top />
   
   
-      <!-- フッター -->
+    <!-- フッター -->
       <v-container fluid>      
         <v-footer color="white" app>
-          <v-layout justify-center>
+          <v-row justify-center>
             <v-card class="mx-auto px-5" outlined tile>
-              <v-layout justify-center>
+              <v-row justify-center>
                 <v-list class="mx-5 my-2 px-5">
                   500 yen/month
                 </v-list>
                 <div class="my-3 mx-1">
                   <v-btn class="px-5" small color="blue lighten-1" dark>E N T R Y</v-btn>
                 </div>
-              </v-layout>
+              </v-row>
             </v-card>
-          </v-layout>
+          </v-row>
         </v-footer>
       </v-container>   
   </v-app>
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,5 +1,32 @@
 <template>
-  <v-container>
-    <h1>This is an top page</h1>
-  </v-container>
+  <v-content>
+    <v-container>
+      <!-- Top部分 -->
+      <v-row class="my-5" justify="center" align-content="center">
+        <div style="text-align:center">
+          <!-- <h2 style="border-bottom:solid 1px #333;">サービス紹介と規約</h2> -->
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/7GkOtwds3P0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h2 >入会条件・注意事項</h2>
+        </div>
+      </v-row>
+      <!-- about部分 -->
+      <v-row class="my-5" justify="center" align-content="center">
+        <div>
+          <p>紹介動画</p>
+          
+        </div>
+      </v-row>
+      
+      <v-row class="my-5" justify="center" align-content="center">
+        <div>
+          サービスの魅力紹介
+        </div>
+      </v-row>  
+      <v-row class="my-5" justify="center" align-content="center">
+        <div>
+          登録の流れ説明
+        </div>
+      </v-row>        
+    </v-container>
+  </v-content>
 </template>
