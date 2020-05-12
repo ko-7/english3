1adc5e53077fbe83918d4da37cfcc7ff36e0e88d
ko-7 <shimura21ngs@gmail.com>
2020-05-12 11:36:17 +0900

    practice gridsystem

--- src/App.vue
+++ src/App.vue
@@ -14,23 +14,36 @@
     </div>
 
     <!-- 以下コンテンツ -->
+    <v-container class="my-5 py-5">
+    <v-list-item link to="/">
+      <v-list-item-content>
+        <v-list-item-title>home</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item link to="/top">
+      <v-list-item-content>
+        <v-list-item-title >top</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    </v-container>
+    <router-view/>
     <Top />
   
   
     <!-- フッター -->
     <v-footer color="white" app class="my-0 py-0">
-      <v-card style="border:solid 1px #DDDDDD;" flat class="mx-auto px-5 my-0 py-0" align="center">
-        <v-container class="mx-5 px-5 my-0 py-0">
-          <v-row class="mx-5 my-0 py-0">
-            <v-col align="center" class="mx-5 my-0 py-3">
+      
+        <v-container class="my-0 py-0" style="border:solid 1px #DDDDDD;">
+          <v-row class="my-0 py-0">
+            <v-col cols="6" align="center" class="my-0">
               <v-text class="title">500 yen/month</v-text>
             </v-col>
-            <v-col align="center" class="mx-5 px-5 my-0 py-3">
+            <v-col cols="6" align="center" class="my-0">
               <v-btn class="title px-5" block color="blue lighten-1" dark>E N T R Y</v-btn>
             </v-col>
           </v-row>
         </v-container>
-      </v-card>
+      
     </v-footer>
   
   </v-app>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container id="container-full">
+    <v-layout>
+      <v-flex xs12>
+        <v-img src="https://www.symph.co/static/img/banner-illus.svg"></v-img>
+      </v-flex>
+    </v-layout>
+    <v-layout text-xs-center mt-5 px-3 py-3 id="about">
+      <v-flex xs12>
+        <span class="display-2 font-weight-bold">About Me</span>
+      </v-flex>
+    </v-layout>
+    <v-layout mt-3 px-5 class="text-xs-center" row wrap>
+      <v-flex xs12>
+        <v-avatar size="250" color="grey lighten-4">
+          <img
+            src="https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/50988163_10213734001079444_2817855097181044736_n.jpg?_nc_cat=104&_nc_ht=scontent-nrt1-1.xx&oh=d558e14acbf714985470fb4b740c35a8&oe=5D357E65"
+            alt="avatar"
+          >
+        </v-avatar>
+        <p class="headline font-weight mt-2">Gwen Danielle Merida</p>
+        <p
+          class="title font-weight-thin mt-3"
+        >Hello I'm a Full Stack Web Developer and iOS Developer from Philippines. Have 5 years work experience in IT Industry.</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
